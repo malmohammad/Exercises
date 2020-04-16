abae8fa55e7c70d9b3947e4dbb430473270b45d1
malmohammad <mak_almo@yahoo.com>
2020-04-15 22:31:06 -0400

    my first commit

---
+++ Ex 6/Ex 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Casino! Roll the dice? (y/n)");
+            input = Console.Readline("6");
+
+
+        }
+    }
+}
---
+++ Ex 6/Exercise 6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+
+        static string tesetResponse = "";
+
+        static void Main(string[] args)
+        {
+            string userResponse;
+            do
+            { 
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Would you like to continue y/n");
+                userResponse = Console.ReadLine(); 
+            } while (userResponse == "y");
+
+            Console.WriteLine("Goodbye!");
+
+        }
+
+
+    }
+}
